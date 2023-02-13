b39ab79bd6a8cc0626582bc4bc293b45be86f56c
Rafael Folco <rafaelfolco@gmail.com>
2023-02-13 12:50:48 -0300

    Add unit tests
    By adding unit tests we enable gradual changes and enhancements
    to the tools without breaking the code. This change adds simple
    tests to the get-json-config utility as part of a bigger change to
    the all-in-one json implementation.

---
+++ bin/tests/test-get-json-config.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import pytest
+import json
+import getjsonconfig
+import re
+
+class TestGetJsonConfig:
+
+    # helper function to read stream from file
+    def _load_file(self, filename):
+        with open("tests/JSON/" + filename, "r") as file:
+            data = file.read().rstrip("\n")
+        return data
+
+    # fixture to load json object from file
+    @pytest.fixture(scope="function")
+    def load_json_file(self, request):
+        return getjsonconfig.load_json_file("tests/JSON/" + request.param)
+
+    """Test if json_to_stream converts endpoint block to a stream"""
+    @pytest.mark.parametrize("load_json_file",
+                             [ "input-oslat-k8s.json" ], indirect=True)
+    def test_json_to_stream_endpoints(self, load_json_file):
+        endpoints_stream = getjsonconfig.json_to_stream(load_json_file, "endpoint", 0)
+        expected_stream = self._load_file("output-oslat-k8s-endpoints.stream")
+
+        assert endpoints_stream == expected_stream
+
+    """Test if json_to_stream converts tags block to a stream"""
+    @pytest.mark.parametrize("load_json_file",
+                             [ "input-oslat-k8s.json" ], indirect=True)
+    def test_json_to_stream_tags(self, load_json_file):
+        tags_stream = getjsonconfig.json_to_stream(load_json_file, "tags", 0)
+        expected_stream = self._load_file("output-oslat-tags.stream")
+
+        assert tags_stream == expected_stream
+
+    """Test if json_to_stream converts passthru-args block to a stream"""
+    @pytest.mark.parametrize("load_json_file",
+                             [ "input-oslat-k8s.json" ], indirect=True)
+    def test_json_to_stream_passthru(self, load_json_file):
+        passthru_stream = getjsonconfig.dump_json(load_json_file, "passthru-args")
+        expected_stream = self._load_file("output-oslat-passthru-args.stream")
+
+        assert passthru_stream == expected_stream
+
+    """Test if load_json_file returns a dict dump_json returns a str"""
+    @pytest.mark.parametrize("load_json_file",
+                             [ "input-oslat-k8s.json" ], indirect=True)
+    def test_dump_json_mvparams(self, load_json_file):
+        assert type(load_json_file) == dict
+        input_json = getjsonconfig.dump_json(load_json_file, "mv-params")
+        assert type(input_json) == str
+
+    """Test if json_to_stream returns None for invalid index"""
+    @pytest.mark.parametrize("load_json_file",
+                             [ "input-oslat-k8s.json" ], indirect=True)
+    def test_json_to_stream_endpoints_invalid_idx(self, load_json_file, capsys):
+        input_json = getjsonconfig.json_to_stream(load_json_file, "endpoint", 1)
+        out, err = capsys.readouterr()
+        assert 'Invalid index' in out
+        assert input_json is None
+
